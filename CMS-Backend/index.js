d4565e82551e44c5a8d3267b8ca42b0934c2ed06
nishi147 <nsk110761@gmail.com>
2026-04-21 01:34:08 +0530

    cxvds

--- CMS-Backend/index.js
+++ CMS-Backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://hcm-8e7v.vercel.app/',
     credentials: true
 }));
 
