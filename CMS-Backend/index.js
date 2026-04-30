8ad17c23006af5900509ef3efaea4b3446a35142
nishi147 <nsk110761@gmail.com>
2026-04-30 08:47:24 +0530

    index.js env chnaged

--- CMS-Backend/index.js
+++ CMS-Backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL || 'https://hcm-8e7v.vercel.app','http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL || 'https://hcm-8e7v.vercel.app'],
     credentials: true
 }));
 
